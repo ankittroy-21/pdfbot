819d4ccbdbcbf54467733233ab555633c0299c82
ankittroy-21 <oxilic05701@gmail.com>
2025-12-16 21:28:55 +0530

    Update configuration with API credentials and adjust .gitignore

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
-API_ID = ""  # Your API ID from https://my.telegram.org/apps
-API_HASH = ""  # Your API Hash from https://my.telegram.org/apps
-BOT_TOKEN = ""  # Your bot token from @BotFather
+API_ID = "5954976"  # Your API ID from https://my.telegram.org/apps
+API_HASH = "1927f462757cdf43a9abcbac129d029d"  # Your API Hash from https://my.telegram.org/apps
+BOT_TOKEN = "8569708732:AAGWDIeiMzkmPcvdlEeHjcH1yNjuST5TBjU"  # Your bot token from @BotFather
